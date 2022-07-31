54ee5bfe28d461a4ec852652a5a1a3688ac2de4d
PatelManthan28 <110323571+PatelManthan28@users.noreply.github.com>
2022-07-31 16:50:56 +0530

    Add files via upload

---
+++ p2main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class p2main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        p2 m = new p2();
+        System.out.println("Enter the string:");
+        String S = sc.nextLine();
+        m.startoz(S);
+        System.out.println("Program done by 21CE098 MANTHAN PATEL");
+    }
+}
