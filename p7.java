b7e6523c51911abf07f015ce15073f2e62e016cf
PatelManthan98 <110323571+PatelManthan98@users.noreply.github.com>
2022-07-31 19:39:18 +0530

    Add files via upload

---
+++ p7.java
@@ -0,0 +1,27 @@
+public class p7{
+    private static final String str = " ";
+    //public static void main(String[] args)
+    void method()
+    {
+        int num= 0;
+        for (int row = 0; row <= 7; row++) {
+
+            for (int column = 1; column <= 7 - row; column++) {
+                System.out.printf("%4s", str);
+            }
+
+            for (int column = 0; column <= row; column++) {
+                num = (int) Math.pow(2, column);
+
+                System.out.printf("%4d", num);
+            }
+
+            for (int column = row - 1; column >= 0; column--) {
+                num = (int) Math.pow(2, column);
+
+                System.out.printf("%4d", num);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ p7main.java
@@ -0,0 +1,8 @@
+public class p7main
+{
+    public static void main(String[] args) {
+        p7 ab = new p7();
+        ab.method();
+        System.out.println("Program done by 21CE098 MANTHAN PATEL");
+    }
+}
