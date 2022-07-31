d644953abaff2575026268daf566a82d646ef906
PatelManthan98 <110323571+PatelManthan98@users.noreply.github.com>
2022-07-31 18:03:22 +0530

    Add files via upload

---
+++ p4main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class p4main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array:");
+        int p = sc.nextInt();
+        p4 k = new p4(p);
+        System.out.println("Program by 21CE098 MANTHAN");
+    }
+}
