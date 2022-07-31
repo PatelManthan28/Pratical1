c1bd7542a0922a826c4e00ac5eaa417650686701
PatelManthan98 <110323571+PatelManthan98@users.noreply.github.com>
2022-07-31 19:54:34 +0530

    Add files via upload

---
+++ p8.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class p8
+{
+        void result()
+        {
+            Scanner scan = new Scanner (System.in);
+
+            String[] answers = { "D","B","D","C","C","D","A","E","A","D"};
+
+            String[][] list = new String[8][10];
+
+            System.out.println("Fill in the eight student's answers to 10 questions" );
+
+            for (int i =0;i<list.length;i++)
+            {
+                int count =0;
+                for (int j=0;j<list[i].length;j++)
+                {
+
+                    list[i][j] = scan.next();
+
+                    if(list[i][j].equalsIgnoreCase(answers[j]))
+                        count++;
+                }
+                System.out.println("Student's " + i + " correct answers are " + count);
+            }
+        }
+    }
+
+
---
+++ p8main.java
@@ -0,0 +1,8 @@
+public class p8main
+{
+    public static void main(String[] args) {
+        p8 a = new p8();
+        a.result();
+        System.out.println("Program done by 21CE098 MANTHAN PATEL");
+    }
+}
